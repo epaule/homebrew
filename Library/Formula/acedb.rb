f1e64a35aa415cc0dee4c97601e065e73fa485f0
Michael Han <mh6@mib101665i.(none)>
2010-10-14 10:34:59 +0100

    changed the comments
    clarifies the gtk+ and acembly issues

--- Library/Formula/acedb.rb
+++ Library/Formula/acedb.rb
@@ -1,6 +1,5 @@
 require 'formula'
 
-# would work, if it would work
 class Acedb <Formula
   url 'ftp://ftp.sanger.ac.uk/pub2/wormbase/data/acedb-4.9.54.tar.bz2'
   homepage 'http://www.acedb.org'
@@ -15,11 +14,13 @@ class Acedb <Formula
 	
 	system 'chmod -R 0755 wmake'
 	system 'chmod 0755 makefile'
-	
+
+	# gtk+ path is dependent on homebrew
 	inreplace 'wmake/DARWIN_4_DEF' do |s|
 	  s.change_make_var! 'PKGPATH',"-local #{HOMEBREW_PREFIX}"
 	end
 	
+	# acembly doesn't compile (yet), so removed from install target
 	inreplace 'makefile' do |s|
 	  s.change_make_var! 'INSTALL_BIN','$(ACE_BIN) $(ALIGN_BIN) $(OTHER_BIN) $(TOOLS_BIN)'
 	end
