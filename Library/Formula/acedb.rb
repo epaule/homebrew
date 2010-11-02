480c446ba0beefea7f5b3c08f7f0f9f7dc3dd665
Michael Paulini <mh6@sanger.ac.uk>
2010-11-02 13:41:21 +0000

    cleaned up a bit

--- Library/Formula/acedb.rb
+++ Library/Formula/acedb.rb
@@ -8,24 +8,26 @@ class Acedb <Formula
   depends_on 'gtk+'
 
   def install
+    
+    #custome compile environments to build on Darwin
     ENV['ACEDB']=prefix
     ENV['ACEDB_MACHINE']='DARWIN_4'
 	
 	
-	system 'chmod -R 0755 wmake'
-	system 'chmod 0755 makefile'
+	  chmod_R 0755, 'wmake'
+	  chmod 0755, 'makefile'
 
-	# gtk+ path is dependent on homebrew
-	inreplace 'wmake/DARWIN_4_DEF' do |s|
-	  s.change_make_var! 'PKGPATH',"-local #{HOMEBREW_PREFIX}"
-	end
+	  # gtk+ path is dependent on homebrew
+	  inreplace 'wmake/DARWIN_4_DEF' do |s|
+	    s.change_make_var! 'PKGPATH',"-local #{HOMEBREW_PREFIX}"
+	  end
 	
-	# acembly doesn't compile (yet), so removed from install target
-	inreplace 'makefile' do |s|
-	  s.change_make_var! 'INSTALL_BIN','$(ACE_BIN) $(ALIGN_BIN) $(OTHER_BIN) $(TOOLS_BIN)'
-	end
+	  # acembly doesn't compile (yet), so removed from install target
+	  inreplace 'makefile' do |s|
+	    s.change_make_var! 'INSTALL_BIN','$(ACE_BIN) $(ALIGN_BIN) $(OTHER_BIN) $(TOOLS_BIN)'
+	  end
 
-	system 'make'
+	  system 'make'
     system 'make install'
   end
 end
