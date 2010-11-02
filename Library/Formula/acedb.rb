140d4e847ca94befb0fec4fab0d4069478fbebca
Michael Paulini <mh6@sanger.ac.uk>
2010-11-02 13:47:50 +0000

    changed tabs to whitespaces

--- Library/Formula/acedb.rb
+++ Library/Formula/acedb.rb
@@ -12,22 +12,22 @@ class Acedb <Formula
     #custome compile environments to build on Darwin
     ENV['ACEDB']=prefix
     ENV['ACEDB_MACHINE']='DARWIN_4'
-	
-	
-	  chmod_R 0755, 'wmake'
-	  chmod 0755, 'makefile'
 
-	  # gtk+ path is dependent on homebrew
-	  inreplace 'wmake/DARWIN_4_DEF' do |s|
-	    s.change_make_var! 'PKGPATH',"-local #{HOMEBREW_PREFIX}"
-	  end
-	
-	  # acembly doesn't compile (yet), so removed from install target
-	  inreplace 'makefile' do |s|
-	    s.change_make_var! 'INSTALL_BIN','$(ACE_BIN) $(ALIGN_BIN) $(OTHER_BIN) $(TOOLS_BIN)'
-	  end
 
-	  system 'make'
+    chmod_R 0755, 'wmake'
+    chmod 0755, 'makefile'
+
+    # gtk+ path is dependent on homebrew
+    inreplace 'wmake/DARWIN_4_DEF' do |s|
+      s.change_make_var! 'PKGPATH',"-local #{HOMEBREW_PREFIX}"
+    end
+
+    # acembly doesn't compile (yet), so removed from install target
+    inreplace 'makefile' do |s|
+      s.change_make_var! 'INSTALL_BIN','$(ACE_BIN) $(ALIGN_BIN) $(OTHER_BIN) $(TOOLS_BIN)'
+    end
+
+    system 'make'
     system 'make install'
   end
 end
